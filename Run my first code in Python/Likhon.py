057df4c72844a86f77f6b7d69dafb5050067f609
Likhon-25 <likhonmondol327@gmail.com>
2026-06-17 01:23:09 +0600

    Explorig Arithmetic Operator in Python

---
+++ Run my first code in Python/Likhon.py
@@ -0,0 +1,46 @@
+# print("Likhon Mondol")
+# print("Hi I'm Likhon Mondol \n I read in Diploma in Engineering")
+
+# a = 10
+# b = 33
+
+# print(a)
+# a = 22
+# print(a)
+
+
+
+# print("Hi! Welcome to Python")
+# name = input("Enter Your Name :")
+# print("Your Name is:", name)
+# Mother = input("Enter Your Mother Name :")
+# print("Your Mother Name is:", Mother)
+# Father = input("Enter Your Father name :" )
+# print("Your Father Name is:", Father)
+# brother = input( "Enter Your Brother name :" )
+# print("Your Brother Name is:", brother)
+
+# print(name, Mother, Father, Brother)
+
+
+#.............. Type Convertion / Casting ............................
+# # Explicit (Manual Convertion)
+# a = float(15)
+# print(a)
+
+# # Implicit (Automatic Convertion)
+# b = 10 + 10.5
+# print(b)
+
+
+# a = 55
+# print(type(a))
+
+
+
+# Statement
+a = 34
+b = 23
+x = a + b
+
+
---
+++ String & Conditional Statements/likhon.py
@@ -0,0 +1,22 @@
+a = 10
+b = 20
+c = a + b
+# print(c)
+
+a = 7
+b = 3
+c = a ** b
+# print(c)
+
+
+# Take two number as input, and show their average.
+a = input("Enter the vlaue of A : ")
+# print(type(a))
+a = int(a)
+# print(type(a))
+b = input("Enter the vlaue of B : ")
+# or
+# b = int(input("Enter the vlaue of B :"))
+b = int(b)
+c = (a + b) / 2
+print("Average is : ",c)
