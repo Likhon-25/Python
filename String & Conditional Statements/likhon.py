6c5130dce2782a566ad1885e8fceca57f9698588
Likhon-25 <likhonmondol327@gmail.com>
2026-06-17 10:43:30 +0600

    explore round, abs and locical operator

--- String & Conditional Statements/likhon.py
+++ String & Conditional Statements/likhon.py
@@ -10,13 +10,33 @@ c = a ** b
 
 
 # Take two number as input, and show their average.
-a = input("Enter the vlaue of A : ")
+# a = input("Enter the vlaue of A : ")
 # print(type(a))
 a = int(a)
 # print(type(a))
-b = input("Enter the vlaue of B : ")
+# b = input("Enter the vlaue of B : ")
 # or
 # b = int(input("Enter the vlaue of B :"))
 b = int(b)
 c = (a + b) / 2
-print("Average is : ",c)
+# print("Average is : ",c)
+
+
+
+
+# logical Operator ---- and , or , not
+# print(4 == 4 and 7 < 6)
+
+
+# -------------Round---------------
+# a = 5.34798798
+# b = round(a, 2)
+# print(b)
+
+# --------------abs----------------
+# a = 4
+# or
+# a = -4
+# b = abs(a)  # |5| ==> 5
+# print(b)
+
