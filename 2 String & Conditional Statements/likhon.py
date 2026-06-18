3f65b069660bebcfffa2cdce20350bbe41fe40d7
Likhon-25 <likhonmondol327@gmail.com>
2026-06-18 15:55:49 +0600

    Start loops and Build-in Data Structure

---
+++ 2 String & Conditional Statements/likhon.py
@@ -0,0 +1,95 @@
+a = 10
+b = 20
+c = a + b
+# print(c)
+
+a = 7
+b = 3
+c = a ** b
+# print(c)
+
+
+# Take two number as input, and show their average.
+# a = input("Enter the vlaue of A : ")
+# print(type(a))
+a = int(a)
+# print(type(a))
+# b = input("Enter the vlaue of B : ")
+# or
+# b = int(input("Enter the vlaue of B :"))
+b = int(b)
+c = (a + b) / 2
+# print("Average is : ",c)
+
+
+
+
+# logical Operator ---- and , or , not
+# print(4 == 4 and 7 < 6)
+
+
+# -------------Round---------------
+# a = 5.34798798
+# b = round(a, 2)
+# print(b)
+
+# --------------abs----------------
+# a = 4
+# or
+# a = -4
+# b = abs(a)  # |5| ==> 5
+# print(b)
+
+
+# String
+
+# a = "Hello World"
+# print(a[2])
+
+age = 20
+# print("I'm", age , "years old now")
+# print(f"I'm {age} years old")
+
+a = "  Hi What's UP!  "
+# print(a.upper())
+# print(a.lower())
+# print(a.strip())
+
+
+
+
+# --------------Decition--------------------
+
+# age = 50
+# if age > 50:
+#     print("Bure hoya geso dadu!")
+
+
+
+# age = 20
+# if age > 18:
+#     print("Vot deta parba")
+# else:
+#     print("Vot deta parba na")
+
+
+
+
+# Take a number as input and find out if its even or odd
+# num = 22
+# if num % 2 == 0:
+#     print("Number is Even")
+# else:
+#     print("Number is Odd")
+
+
+
+num = -1
+if num > 0:
+    print("Positive")
+elif num < 0:
+    print("Negative")
+else:
+    print("Zero")
+
+
