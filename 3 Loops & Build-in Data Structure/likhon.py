e653c2240c5a30b21ece3f09618bd7a39073908b
Likhon-25 <likhonmondol327@gmail.com>
2026-06-19 16:53:56 +0600

    learn while loop and some problem solve

---
+++ 3 Loops & Build-in Data Structure/likhon.py
@@ -0,0 +1,62 @@
+# count = 1
+
+# while count <= 10 : 
+    # print(count)
+#     count += 1
+
+ # print("The End")
+
+
+# -----------------------------------------------
+
+count = 1
+add = 0 
+
+while count <= 10:
+    add = add + count
+    # print(add)
+    count += 1
+
+# print(f"Total sum : {add} ")
+# print("The End")
+
+# -----------------------------------------------
+
+
+num = 1        
+mult = 1        
+
+while num <= 5: 
+    mult = mult * num 
+    # print(mult)
+    num += 1
+
+# print(f"Multiplication Total : {mult}")
+# print("The End")
+
+# --------------------------------------------------
+
+i = 1
+
+while i < 6:
+    # print(i)
+
+    if i == 3:
+        break
+    i += 1
+
+# -------------------
+
+i = 0
+while i < 6:
+    i += 1
+
+    if i == 3:
+        continue
+    # print(i)
+
+
+# ----------------------------------------------------
+
+
+
