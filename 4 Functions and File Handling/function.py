956e580368a112368cc03fdf3525233b5dc5031d
Likhon-25 <likhonmondol327@gmail.com>
2026-06-21 01:44:52 +0600

    Learn python function

--- 4 Functions and File Handling/function.py
+++ 4 Functions and File Handling/function.py
@@ -1,7 +1,42 @@
-def make_breakfast():
-    print("Mking Tost")
-    print("Appling Butter")
-    print("Making Tea")
+# def make_breakfast():
+    # print("Mking Tost")
+    # print("Appling Butter")
+    # print("Making Tea")
 
-make_breakfast()
-make_breakfast()
+# make_breakfast()
+
+# --------------------------------------
+
+# def make_breakfast(a):  #Parameter
+#     print(a)
+#     print("Make Tea")
+
+# make_breakfast(10) #Argument
+
+# --------------------------------------
+
+# def make_breakfast(a):  #Parameter
+#     print(a)
+#     print("Make Tea")
+#     return 20
+
+# b = make_breakfast(10) #Argument
+# print(b)
+# c = make_breakfast(30) #Argument
+# print(c)
+
+# --------------------------------------
+
+
+# def sum(a, b):
+#     return a + b
+# total = sum(10 , 20)
+# print("Total Summation : ",total)
+# # -----------------same concept---------------------
+# def sum(a, b):
+#     return a + b
+# t = sum
+# total = t(10,20)
+# print("Total Summation : ",total)
+
+
